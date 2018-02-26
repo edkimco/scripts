fefcefdb3d1315908c7aaa4b5c2847e9799012fe
the6thm0nth <hoonch3@gmail.com>
2018-02-26 17:55:46 +0900

    [add] 필요없는 기존 레이블 지우는 명령 추가

--- init-labels.sh
+++ init-labels.sh
@@ -4,15 +4,26 @@
 # $2 = USERNAME
 # $3 = PASSWORD
 
-cat ./issues | while read LINE
-
+cat ./src/issues | while read ISSUE
 do
-	name="$(echo $LINE | cut -d';' -f1)"
-	color="$(echo $LINE | cut -d';' -f2)"
+	name="$(echo $ISSUE | cut -d';' -f1)"
+	color="$(echo $ISSUE | cut -d';' -f2)"
 
 	curl \
 	-H "Content-Type: application/json" \
 	-d '{"name": "'"$name"'", "color": "'"$color"'"}' \
 	https://api.github.com/repos/edkimco/$1/labels \
-	-u "$2:$3"
+	-u "$2:$3";
+done
+
+cat ./src/remove-issues | while read ISSUE_TO_REMOVE
+do
+	name="$(echo $ISSUE_TO_REMOVE)"
+
+	curl \
+	-X DELETE \
+	-H "Content-Type: application/json" \
+	"https://api.github.com/repos/edkimco/$1/labels/$name" \
+	-u "$2:$3" \
+	-f;
 done
